3a5f5cc346dc5ac377402af7c66500b5e40106cb
Lucas Henrique Chaves <103153987+lucasxae@users.noreply.github.com>
2022-11-04 16:03:34 -0300

    Add files via upload

---
+++ TEP_Prova2_25/TEP_Prova2_25/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TEP_Prova2_25
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        area objarea = new area();
+        private void btn_calcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               objarea.Altura = double.Parse(txt_altura.Text);  
+               objarea.Bbase = double.Parse(txt_base.Text);
+           
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("Error. \n" + ex.Message);
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+MessageBox.Show(objarea.CalcularArea().ToString());
+
+            if(objarea.CalcularArea() > 5)
+            {
+                MessageBox.Show("O tamanho da área ultrapassou o limite do  tamanho especificado");
+            }
+            else
+            {
+                MessageBox.Show("o tamanho está dentro dos padrões especificados");
+            }
+
+        
+        }
+    }
+}
---
+++ TEP_Prova2_25/TEP_Prova2_25/area.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEP_Prova2_25
+{
+    internal class area
+    {
+        private double altura;
+        private double bbase;
+
+
+        public double Altura
+        {
+            get => altura;
+            set { if (value >= 0 ) { this.altura = value; } else { throw new Exception("O valor da altura  não deve ser menor que 0"); } }
+
+        }
+
+        public double Bbase
+        {
+            get => bbase;
+            set { if (value >= 0 ) { this.bbase = value; } else { throw new Exception("O valor da base não deve ser menor que 0"); } }
+        }
+
+
+        public double CalcularArea()
+        {
+            return this.bbase * this.altura;
+        }
+    }
+}
