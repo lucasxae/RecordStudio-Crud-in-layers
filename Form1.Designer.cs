47ee581b32aa3e6a5b3852ba374aeb5dfe57875b
Lucas Henrique Chaves <103153987+lucasxae@users.noreply.github.com>
2023-06-14 10:07:45 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace RevisaoProva
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNomeMusica = new System.Windows.Forms.TextBox();
+            this.TxtNomeGravadora = new System.Windows.Forms.TextBox();
+            this.txtNomeAutor = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome da Musica";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nome da Gravadora";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nome do Autor";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(135, 11);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 20);
+            this.txtId.TabIndex = 4;
+            // 
+            // txtNomeMusica
+            // 
+            this.txtNomeMusica.Location = new System.Drawing.Point(135, 46);
+            this.txtNomeMusica.Name = "txtNomeMusica";
+            this.txtNomeMusica.Size = new System.Drawing.Size(313, 20);
+            this.txtNomeMusica.TabIndex = 5;
+            // 
+            // TxtNomeGravadora
+            // 
+            this.TxtNomeGravadora.Location = new System.Drawing.Point(135, 77);
+            this.TxtNomeGravadora.Name = "TxtNomeGravadora";
+            this.TxtNomeGravadora.Size = new System.Drawing.Size(313, 20);
+            this.TxtNomeGravadora.TabIndex = 6;
+            // 
+            // txtNomeAutor
+            // 
+            this.txtNomeAutor.Location = new System.Drawing.Point(135, 112);
+            this.txtNomeAutor.Name = "txtNomeAutor";
+            this.txtNomeAutor.Size = new System.Drawing.Size(313, 20);
+            this.txtNomeAutor.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(24, 150);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(541, 157);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(24, 315);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(109, 34);
+            this.btnSalvar.TabIndex = 9;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(240, 315);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(109, 34);
+            this.btnExcluir.TabIndex = 10;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(456, 315);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(109, 34);
+            this.btnSair.TabIndex = 11;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 361);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtNomeAutor);
+            this.Controls.Add(this.TxtNomeGravadora);
+            this.Controls.Add(this.txtNomeMusica);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNomeMusica;
+        private System.Windows.Forms.TextBox TxtNomeGravadora;
+        private System.Windows.Forms.TextBox txtNomeAutor;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using RevisaoProva.BLL;
+using RevisaoProva.DTO;
+
+namespace RevisaoProva
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Criar os objetos bll e dto para serem usados
+        //O nome objbll_musica e objdto_musica são apenas variaveis, então podem ser trocadas por qualquer nome para ser usada depois.
+        BLL_Musica objbll_musica = new BLL_Musica();
+        DTO_Musica objdto_musica = new DTO_Musica();
+
+        //Função para lstar a tabela no DataGridView
+        public void ListarMusicas()
+        {
+            try
+            {
+                dataGridView1.DataSource = objbll_musica.Listar_Musicas();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Função para limpar os campos do TextBoxes
+        public void Limpar()
+        {
+            txtId.Clear();
+            txtNomeAutor.Clear();
+            txtNomeMusica.Clear();
+            TxtNomeGravadora.Clear();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Aqui você deve setar as variaveis do seu DTO por aqui.
+                objdto_musica.Nome = txtNomeMusica.Text;
+                objdto_musica.NomeGravadora = TxtNomeGravadora.Text;
+                objdto_musica.NomeAutor = txtNomeAutor.Text;
+
+                //Passar os valores do DTO colocados acima para serem usados no BLL com o comando que deseja.
+                objbll_musica.Inserir_Musica(objdto_musica);
+                MessageBox.Show("Musica inserida com sucesso!");
+                //Rodar as funções criadas acima para atualizar a tabela e limpar os campos digitados.
+                ListarMusicas();
+                Limpar();
+            }
+            catch(Exception ex)
+            {
+                //Caso dê erro acima, aqui irá fazer o código aparecer a mensagem de erro.
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                objdto_musica.Id = int.Parse(txtId.Text);
+                objbll_musica.Excluir_Musica(objdto_musica);
+                Limpar();
+                ListarMusicas();
+                MessageBox.Show("Musica excluida com sucesso!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            //Fecha a página atual.
+            this.Close();
+        }
+
+        //Clique duas vezes no formulario que irá criar essa função, ela serve para rodar um comando quando a página for aberta.
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Com esse comando a tabela no DataGridView irá ser carregada automaticamente.
+            ListarMusicas();
+        }
+    }
+}
